8818c4e5642f9eced732da01188120b1ea21a7be
ChristinaCode <116737788+ChristinaCode@users.noreply.github.com>
2025-11-19 12:38:46 -0600

    Black-Out Screen

--- godot-game/src/OpeningScene/openingscene.gd
+++ godot-game/src/OpeningScene/openingscene.gd
@@ -1,10 +1,19 @@
 extends AnimationPlayer
 
+@onready var anim = $AnimationPlayer
+@onready var camera = $CutsceneCamera
+@onready var message_label = $CanvasLayer/MessageLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+func show_message(text: String):
+	message_label.text = text
+	message_label.visible = true
+
+func hide_message() -> void:
+	message_label.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
