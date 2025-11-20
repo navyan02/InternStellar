a2ea39ebbd80a06a7d2b6096e0f141335dce7181
ChristinaCode <116737788+ChristinaCode@users.noreply.github.com>
2025-11-20 13:17:50 -0600

    DIALOGUE :D

--- godot-game/src/OpeningScene/openingscene.gd
+++ godot-game/src/OpeningScene/openingscene.gd
@@ -6,7 +6,8 @@ extends AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	play("opening")
+	pass
 
 func show_message(text: String):
 	message_label.text = text
