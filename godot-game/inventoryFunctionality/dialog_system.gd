e694e4c971c4f5f87c05876446f9cad062ca5dbf
ChristinaCode <116737788+ChristinaCode@users.noreply.github.com>
2025-11-20 17:18:51 -0600

    INVASION!!!

--- godot-game/inventoryFunctionality/dialog_system.gd
+++ godot-game/inventoryFunctionality/dialog_system.gd
@@ -2,33 +2,54 @@ extends CanvasLayer
 class_name DialogSystem
 
 @export var display_time: float = 3.0
+@export var typewriter_speed: float = 0.03  # seconds per character
 
 @onready var dialog_panel = $Panel
 @onready var dialog_label = $Panel/MarginContainer/Label
 
 var timer: Timer
+var typing := false
+var full_text := ""
+var typing_index := 0
 
 func _ready():
 	add_to_group("dialog")
-	#dialog_panel.visible = true
 	
 	# Create timer programmatically
 	timer = Timer.new()
 	add_child(timer)
-	timer.timeout.connect(_on_timer_timeout)
 	timer.one_shot = true
 
 func show_message(text: String, duration: float = -1.0):
-	dialog_label.text = text
+	full_text = text
+	dialog_label.text = ""
 	dialog_panel.visible = true
-	print(text)
 	
-	var time = duration if duration > 0 else display_time
-	timer.start(time)
+	typing = true
+	typing_index = 0
+	
+	# STOP the hide timer. We won't use it.
+	timer.stop()
+	
+	_process_typewriter()
 
-func _on_timer_timeout():
-	dialog_panel.visible = false
+func _process_typewriter() -> void:
+	if not typing:
+		return
+	
+	if typing_index < full_text.length():
+		dialog_label.text = full_text.substr(0, typing_index + 1)
+		typing_index += 1
+		
+		await get_tree().create_timer(typewriter_speed).timeout
+		_process_typewriter()
+	else:
+		# Finished typing
+		typing = false
+		# DO NOT start any timer here. Text stays visible until manually hidden.
+		pass
 
 func hide_dialog():
 	dialog_panel.visible = false
 	timer.stop()
+	typing = false
