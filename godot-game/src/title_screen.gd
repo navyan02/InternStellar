eb60b4ffa8aad77f5570be26c7aebfc63342def0
Katie-N <kaitlyn.nordberg@gmail.com>
2025-11-22 23:55:23 -0600

    Made working scene changes and ability to quit game

--- godot-game/src/title_screen.gd
+++ godot-game/src/title_screen.gd
@@ -2,6 +2,8 @@ extends Control
 
 func _on_start_button_pressed() -> void:
 	print("Start Game")
+	get_tree().change_scene_to_file("res://src/OpeningScene/openingscene.tscn")
 
 func _on_exit_pressed() -> void:
 	print("Close game")
+	get_tree().quit()
